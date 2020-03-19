1924345fb7918d77b3ea4f99ee07bdb265c7ac22
gargipatel001 <gargipatel001@gmail.com>
2020-03-19 14:04:48 -0400

    Worked on Roles and Partner Pages on Admin side

---
+++ LoyaltyProgram/Controllers/PartnerController.cs
@@ -0,0 +1,183 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using LoyaltyProgram.DAL;
+using LoyaltyProgram.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoyaltyProgram.Controllers
+{
+    public class PartnerController : Controller
+    {
+        private LoyaltyProgramContext db = new LoyaltyProgramContext();
+
+        // GET: Partner
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+          
+            try
+            {
+                
+               var data = db.Partners.ToList();
+                return Json(data.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+                //throw;
+            }
+           
+           
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create([DataSourceRequest] DataSourceRequest request, Partner partner)
+        {
+            if (ModelState.IsValid)
+            {
+                if (TempData["File"]!=null)
+                {
+                    string fileName = "";
+                    //var img = Convert.ToBase64String((byte[])TempData["File"]);
+                    //Byte[] file = (byte[])TempData["File"]);
+                     HttpPostedFileWrapper file = TempData["File"] as HttpPostedFileWrapper;
+                    if (file!=null)
+                    {
+                        string path = Server.MapPath("~/Content/Images/PromotionPartners/");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        fileName = file.FileName;
+                        file.SaveAs(path + Path.GetFileName(file.FileName));
+                    }
+                    partner.PartnerLogo = "Images/PromotionPartners/" + fileName;
+
+                }
+               
+                db.Partners.Add(partner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Update([DataSourceRequest] DataSourceRequest request, Partner partner)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (TempData["File"]!=null)
+                    {
+                        string fileName = "";
+                        string fullPath = Request.MapPath("~/Content/" + partner.PartnerLogo);
+                       
+                        
+                        //var img = Convert.ToBase64String((byte[])TempData["File"]);
+                        //Byte[] file = (byte[])TempData["File"]);
+                        HttpPostedFileWrapper file = TempData["File"] as HttpPostedFileWrapper;
+                        if (file != null)
+                        {
+                            string path = Server.MapPath("~/Content/Images/PromotionPartners/");
+                            if (!Directory.Exists(path))
+                            {
+                                Directory.CreateDirectory(path);
+                            }
+                            fileName = file.FileName;
+                            file.SaveAs(path + Path.GetFileName(file.FileName));
+                            if (System.IO.File.Exists(fullPath))
+                            {
+                                System.IO.File.Delete(fullPath);
+                            }
+                            TempData["File"] = null;
+                        }
+                        partner.PartnerLogo = "Images/PromotionPartners/" + fileName;
+
+
+                    }
+                    db.Entry(partner).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult CheckPartner(string partnerName)
+        {
+            try
+            {
+                int Count = db.Partners.Where(_ => _.PartnerName == partnerName).Count();
+
+                return Json(Count, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(0, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        public ActionResult Save(IEnumerable<HttpPostedFileBase> files)
+        {
+            // The Name of the Upload component is "files"
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    TempData["File"] = file;
+                    // Some browsers send file names with full path.
+                    // We are only interested in the file name.
+                   // var fileName = Path.GetFileName(file.FileName);
+                    //var physicalPath = Path.Combine(Server.MapPath("~/App_Data"), fileName);
+
+                    // The files are not actually saved in this demo
+                    // file.SaveAs(physicalPath);
+                }
+            }
+
+            // Return an empty string to signify success
+            return Content("");
+        }
+        public ActionResult Remove(string[] fileNames)
+        {
+            // The parameter of the Remove action must be called "fileNames"
+
+            if (fileNames != null)
+            {
+                if (TempData["File"]!=null)
+                {
+                    TempData["File"] = null;
+                }
+                //foreach (var fullName in fileNames)
+                //{
+                //    var fileName = Path.GetFileName(fullName);
+                //    var physicalPath = Path.Combine(Server.MapPath("~/App_Data"), fileName);
+
+                //    // TODO: Verify user permissions
+
+                //    if (System.IO.File.Exists(physicalPath))
+                //    {
+                //        // The files are not actually removed in this demo
+                //        // System.IO.File.Delete(physicalPath);
+                //    }
+                //}
+            }
+
+            // Return an empty string to signify success
+            return Content("");
+        }
+    }
+}
--- LoyaltyProgram/Controllers/RolesController.cs
+++ LoyaltyProgram/Controllers/RolesController.cs
@@ -20,7 +20,8 @@ namespace LoyaltyProgram.Controllers
         // GET: Roles
         public ActionResult Index()
         {
-            return View(db.Roles.ToList());
+           
+            return View();
         }
         public ActionResult Select([DataSourceRequest]DataSourceRequest request)
         {
@@ -28,45 +29,9 @@ namespace LoyaltyProgram.Controllers
             return Json(data.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
         }
     
-        // GET: Roles/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Roles roles = db.Roles.Find(id);
-            if (roles == null)
-            {
-                return HttpNotFound();
-            }
-            return View(roles);
-        }
+       
 
-        // GET: Roles/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Roles/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "RoleId,RoleName,RoleDescription")] Roles roles)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Roles.Add(roles);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(roles);
-        }
-
-        // GET: Roles/Edit/5
+        
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -81,55 +46,53 @@ namespace LoyaltyProgram.Controllers
             return View(roles);
         }
 
-        // POST: Roles/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "RoleId,RoleName,RoleDescription")] Roles roles)
+        
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Update([DataSourceRequest] DataSourceRequest request, Roles roles)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(roles).State = EntityState.Modified;
+                try
+                {
+                    db.Entry(roles).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+            return RedirectToAction("Index");
+        }
+       
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create([DataSourceRequest] DataSourceRequest request, Roles roles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(roles);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(roles);
-        }
 
-        // GET: Roles/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Roles roles = db.Roles.Find(id);
-            if (roles == null)
-            {
-                return HttpNotFound();
-            }
-            return View(roles);
-        }
-
-        // POST: Roles/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Roles roles = db.Roles.Find(id);
-            db.Roles.Remove(roles);
-            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
+       
+        public ActionResult CheckRole(string roleName)
         {
-            if (disposing)
+            try
             {
-                db.Dispose();
+                int Count = db.Roles.Where(_ => _.RoleName == roleName).Count();
+                  
+                return Json(Count, JsonRequestBehavior.AllowGet);
             }
-            base.Dispose(disposing);
+            catch (Exception ex)
+            {
+                return Json(0, JsonRequestBehavior.AllowGet);
+            }
+
         }
     }
 }
